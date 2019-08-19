80bf3e5012b76ffe353f6c72bc7a476fa4652184
A761647 <guillaumemoraine.anais@stagiaresifa.fr>
2019-08-19 11:13:33 +0200

    ok

---
+++ src/main/java/com/RPG/IFA/ProjetFileRouge/ProjetFileRougeApplication.java
@@ -0,0 +1,13 @@
+package com.RPG.IFA.ProjetFileRouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFileRougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFileRougeApplication.class, args);
+	}
+
+}
